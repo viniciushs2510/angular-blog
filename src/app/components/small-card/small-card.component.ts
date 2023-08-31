1fd65b97b4ac1bd9e5a9ba4486deea15219eef49
Vinicius Honorato <viniciushs2510@gmail.com>
2023-08-31 17:41:36 -0300

    Change css

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  marginValue: string = '20px';
+  @Input() hasMargin: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    if(!this.hasMargin) {
+      this.marginValue = '0px';
+    }
   }
 
 }
